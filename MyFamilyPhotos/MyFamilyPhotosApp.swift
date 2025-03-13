18d4b86acafc9498910b9f5ec09c93396f79e291
Larry Shannon <larry@breakawaydesign.com>
2025-03-13 09:50:09 -0700

    Initial Commit

---
+++ MyFamilyPhotos/MyFamilyPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFamilyPhotosApp.swift
+//  MyFamilyPhotos
+//
+//  Created by Larry Shannon on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFamilyPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
